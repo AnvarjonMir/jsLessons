a569f6bbb73c432bc4af6091b8e591a395049f5c
AnvarjonMir <115082661+AnvarjonMir@users.noreply.github.com>
2022-12-07 06:27:05 +0300

    Lobaratory

---
+++ amaliyot/js/js.js
@@ -0,0 +1,30 @@
+"use strict"
+
+const numberOfSeries = +prompt("Nechta serial ko'rdingiz");
+
+const seriesDB = {
+    count: numberOfSeries,
+    series: {},
+    actors: {},
+    genres: [],
+    private: false
+}
+
+console.log(numberOfSeries);
+
+
+
+const serial = prompt("Oxirgi ko'rgan serialingiz"),
+      baho = +prompt("Nechta baho berasiz"),
+      serial1 = prompt("Oxirgi ko'rgan serialingiz"),
+      baho1 = +prompt("Nechta baho berasiz");
+
+
+seriesDB.series[serial] = baho;
+seriesDB.series[serial1] = baho1;
+
+/*console.log(`${serial}: "${baho}"`)
+console.log(`${serial1}: "${baho1}"`)*/
+
+
+console.log(seriesDB);
