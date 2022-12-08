27bf6d57f69d9672ea624f96f839819360ac4dbb
AnvarjonMir <115082661+AnvarjonMir@users.noreply.github.com>
2022-12-08 21:18:30 +0300

    add sikl

---
+++ Sikl/js/js.js
@@ -0,0 +1,38 @@
+"use strict"
+
+
+ // while kak takrorolardek gap 
+//let num = 10;
+
+// First
+/*while(startNum <= 100) {
+    console.log(startNum);
+    startNum++;
+}*/
+
+// second
+/*
+do {  // bajar
+    console.log(startNum);
+    startNum++;
+} while (startNum <= 10);  // manashu vaqt ichida*/
+
+
+
+// Trird  ENG KO'P ISHLATILADIGAN YO'LI
+
+/*for (let i = 1; i <= 8; i++) {
+    console.log(num);
+    num++;
+}*/
+
+
+
+for (let i = 1; i <= 10; i++) {
+
+    if (i === 8) {
+        //break; // shu joyda to'xtatadi
+        continue; // belgilangan sonni o;tkazib yuboradi
+    }
+     console.log(i)
+}
