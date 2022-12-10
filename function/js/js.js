fd18f51cf44e4865b4f7dda915b364d05cfccb0b
AnvarjonMir <115082661+AnvarjonMir@users.noreply.github.com>
2022-12-11 00:27:48 +0300

    add funnction

--- function/js/js.js
+++ function/js/js.js
@@ -1,30 +1,99 @@
-"use strict"
-
-console.log("Open modal");
+// "use strict"
 
 
-// function calc(a, b) {
-//     console.log(a + b);
-// }
 
-// calc(10, 5);
-// calc(9, 10);
+// function funksiya() {code ..}
 
-// funksiya orqali ikkita sonni qo'shevchi dastur
+// funcsiya() // chaqirish uchun 
 
 
-            // AVVAL funcsiya ichi keyin esa tasqarisi chiqariladi
+/*      // IKKITA SONNI QO'SHISH DASTURI
+
+function calc(a, b) {
+    console.log(a + b);
+}
+
+calc(10, 5);
+calc(9, 10);
+
+// funksiya orqali ikkita sonni qo'shevchi dastur  */
+
+
+ /*           // AVVAL funcsiya ichi keyin esa tasqarisi chiqariladi
+
 let age = 30;
 function sayMyName(name, surname) {
     age = 20; // o'zgaruvchilarni faqat funcsiyani ichida chaqiriahimiz keerak
-
+    // ichkaridagi age = 20 tasgqaridagi qiymatni o;zgartirdi
     console.log(`My name is ${name} ${surname}. My age ${age}.`);
-    console/lpg(age);
+    console.log(age);
 }
 
 sayMyName("Anvar", "Mirzoakhmedov"); // string formaida // har doim ketma ketlikda
 
-console.log(age);
+console.log(age); */
+
+
+/*        // IKKITA SONNI QO'SHISH
+
+function calc(a, b) {
+    return a + b; // returndan keyin hech narsa yozib bo'lmaydi
+    // return har qaynday mao'lumot turi bo'lishi mumkin
+    // returndagi qiymatni chiqarish uchun uni chaqirish kerak
+}
+
+// console.log(calc(5,6));
+// console.log(calc(8,8));
+
+
+const sumOfAandB = calc(10, 5); // global o'zgaruchiga olishimiz va xohlagancha ishlatishimiz mumkin
+console.log(sumOfAandB);     */
+
+
+ /*      // FUNCSIYA TURLARINING ENG ASOSIYSI UCHTA
+// --- FUNCTION DECLORATION
+// --- FUNCTION EXPRESSION
+// --- ARROW FUNCTION       */
+
+
+/*       // --- FUNCTION DECLORATION
+// BOSHQA FUNCSIYALARDAN FARQI UNDAGIMA'LUMOTLARNI CAQIRISH UCHUN FUNCSIYANI TAGIGA YOZISHIMIZ SHART EMAS
+
+/*              FUNCTION FOO () {
+             CODE...
+                }
+
+*/
+
+
+/*        // --- FUNCTION EXPRESSION
+// FUNCSIYANI KEYIN CHAQIRISH  KERAK
+
+        const uzgaruvchi = funcsiya(){        // o'zgaruvchiqa tenglab oslish va u orqali chaqirish  // faqat sal boshqacahroq
+                console.log("Hello world")
+        }
+
+        uzgaruvchi();            
+ 
+        console.log(); 
+  */
+ 
+        
+/*                // ARROW FUNCTION
+
+ const calc = (a ,b) =>  RETURN  a + b;
+
+ console.log(calc(4 , 6));
+
+
+const calc = (a) => {
+    console.log("!");
+    return a;
+};  */
+
+       
+
+
 
 
 
