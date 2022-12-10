f12fe139211f16cffc7668b2a397eb1f8fbc1888
AnvarjonMir <115082661+AnvarjonMir@users.noreply.github.com>
2022-12-11 00:28:52 +0300

    i add

---
+++ Metod va Xususiyatlar/js/js.js
@@ -0,0 +1,64 @@
+"use strict"
+/* BOSHLANISHII
+const user = "Anvar";
+console.log(user.length); // uzunligini o'lchab beradi
+
+const arr = [1, 2 ,3, 4];
+console.log(arr.length); // arrayda hozir 4 ta element bor
+*/
+
+/*    // BITTA HARFNI AJRATIB OLISH  
+const user = "Anvar";
+console.log(user[0]); // bitta harfini ajraib olish uchun
+
+const arr = [1 ,2 ,3 , 4];
+console.log(arr[3]); // array uchun  */
+
+/* UPER lower Case
+const user = "Money";
+console.log(user);
+console.log(user.toUpperCase()); // hamma harflar bosh harfda
+const uperCase = user.toUpperCase();
+console.log(uperCase);
+console.log(user.toLowerCase()) // hamma harhlar kichik harfda
+*/
+
+/* INDEX YANI QAERDA TURGANINI ANIQLASH
+const greeting = "Hello world";
+console.log(greeting.indexOf("z")); // matndagi harfni indexini topib beradi
+console.log(greeting.indexOf("H")); // ishlagan varianti
+*/
+
+/* QIRQIB OLISH slice
+const salomlashish = "Salom dunyo";
+console.log(salomlashish.slice(0, 6)); //  index yordamida qirqib oldik
+console.log(salomlashish.slice(6, 11)); // dunyo so'zini qirqib berdi
+console.log(salomlashish.slice(6)); // 6 dan boshlab oxirigacha qirqib beradi
+console.log(salomlashish.slice(-6, -1)) // teskarisiga qirqib olish
+*/
+
+/*   // QIRQIB OLISH substring
+
+const salom = "Salom maktab";
+console.log(salom.substring(6, 12));  // faqat minus qiymat qabul qilmaydi xolos
+
+*/
+
+/*       // SUBSTR
+const kesish = "Hammaga salom";
+console.log(kesish.substr(8, 5)); // 8 index, 5 harflar soni
+
+*/
+
+/* NUMBER USTIDA AMALLAR
+const num = 8.8;
+console.log(Math.round(num)); // butun songa eng yaqin son
+
+const borderWidth = "13.8px";
+// console.log(parseInt(borderWidth)); // stringdagi butun sonni ajratib olib harflarni li btashlaydi
+console.log(parseFloat(borderWidth)); // stringdagi sonni to'liq ajratib beradi
+*/
+
+
+
+
