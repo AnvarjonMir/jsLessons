21e5e6ad3923ad23fca0cfcd3825f4da609728a1
AnvarjonMir <115082661+AnvarjonMir@users.noreply.github.com>
2022-12-10 14:59:01 +0300

    ADD

--- amaliyot/js/js.js
+++ amaliyot/js/js.js
@@ -2,6 +2,8 @@
 
 const numberOfSeries = +prompt("Nechta serial ko'rdingiz");
 
+
+
 const seriesDB = {
     count: numberOfSeries,
     series: {},
@@ -10,18 +12,85 @@ const seriesDB = {
     private: false
 }
 
+
+// let i = 0;
+
+// do {
+//     const serial = prompt("Oxirgi ko'rgan serialingiz"),
+//           baho = +prompt("Necha baho berasiz");
+//           i++;
+
+//     if(serial != null && baho != null && serial != "" && baho != ""){
+//         seriesDB.series[serial] = baho;
+//         console.log("done");
+//     }else {
+//         console.log("error");
+//         i--;
+//     }
+
+//     seriesDB.series[serial] = baho;
+   
+// } while(i < 2);
+
+
+
+
+
+
+// while(i < 2) {
+//     const serial = prompt("Oxirgi ko'rgan serialingiz"),
+//           baho = +prompt("Necha baho berasiz");
+//           i++;
+
+//     if(serial != null && baho != null && serial != "" && baho != ""){
+//         seriesDB.series[serial] = baho;
+//         console.log("done");
+//     }else {
+//         console.log("error");
+//         i--;
+//     }
+
+//     seriesDB.series[serial] = baho;
+// }
+
+
+
+
+for(let i = 0; i < 2; i++) {
+    const serial = prompt("Oxirgi ko'rgan serialingiz"),
+          baho = +prompt("Necha baho berasiz");
+          
+
+    if(serial != null && baho != null && serial != "" && baho != ""){
+        seriesDB.series[serial] = baho;
+        console.log("done");
+    }else {
+        console.log("error");
+        i--;
+    }
+
+    seriesDB.series[serial] = baho;
+}
+
+
+
+
 console.log(numberOfSeries);
 
 
+if(numberOfSeries.count < 5) {
+    console.log("Kam serial ko'ribsiz");
+} else if(numberOfSeries <= 5 && numberOfSeries < 10) {
+    console.log("Siz klassik serialchi ekansiz");
+} else if(numberOfSeries >= 10) {
+    console.log("Siz serialchi zvezda ekansiz");
+} else {
+    console.log("Error");
+}
 
-const serial = prompt("Oxirgi ko'rgan serialingiz"),
-      baho = +prompt("Nechta baho berasiz"),
-      serial1 = prompt("Oxirgi ko'rgan serialingiz"),
-      baho1 = +prompt("Nechta baho berasiz");
 
 // bjektga qo'shish
-seriesDB.series[serial] = baho;
-seriesDB.series[serial1] = baho1;
+
 
 /*console.log(`${serial}: "${baho}"`)
 console.log(`${serial1}: "${baho1}"`)*/
